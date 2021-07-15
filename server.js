174407f31b1ee27514761a85c312ab3fa1e9f19d
Daryl Nauman <darylnauman@gmail.com>
2021-07-15 19:53:47 -0400

    deploying

--- server.js
+++ server.js
@@ -16,7 +16,13 @@ app.use(express.json());
 app.use(express.static("public"));
 app.use(routes);
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true, useFindAndModify: false });
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout",
+{ 
+  useNewUrlParser: true,
+  useFindAndModify: false,
+  userCreateIndex: true,
+  useFindAndModify: false 
+});
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
